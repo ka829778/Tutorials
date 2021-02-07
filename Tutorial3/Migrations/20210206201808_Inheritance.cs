1f185a6b890e3ad6d4c2d3636012d75525b9bcf6
ka829778 <70986702+ka829778@users.noreply.github.com>
2021-02-07 12:58:02 -0600

    Add files via upload

---
+++ Tutorial3/Migrations/20210206201808_Inheritance.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContosoUniversity.Migrations
+{
+    public partial class Inheritance : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Student");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Student");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Instructor");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Instructor");
+
+            migrationBuilder.CreateTable(
+                name: "Person",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    LastName = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    FirstName = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Person", x => x.ID);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Instructor_Person_ID",
+                table: "Instructor",
+                column: "ID",
+                principalTable: "Person",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Student_Person_ID",
+                table: "Student",
+                column: "ID",
+                principalTable: "Person",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Instructor_Person_ID",
+                table: "Instructor");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Student_Person_ID",
+                table: "Student");
+
+            migrationBuilder.DropTable(
+                name: "Person");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Student",
+                type: "TEXT",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Student",
+                type: "TEXT",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Instructor",
+                type: "TEXT",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Instructor",
+                type: "TEXT",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
